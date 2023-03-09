e0941b4541de073006468fd9681dd35029823854
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2023-03-09 11:11:28 +0900

    better view in iphone

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const htmlTemplate = `
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Upload</title>
 </head>
 <body>
